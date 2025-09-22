f8b74f0e4d86350e48b36a52ee4d1436ee9f4bb4
Vincent.Dimov <vincent.dimov@hotmail.se>
2025-09-22 15:24:16 +0200

    ff

---
+++ app.js
@@ -0,0 +1,19 @@
+//VARIABLER FÖR INPUT AV OLIKA ELEMENT
+const form = document.getElementById('form')
+const namnInput = document.getElementById('namn')
+const epostInput = document.getElementById('e-post')
+const telInput = document.getElementById('tel')
+const meddelandeInput = document.getElementById('meddelande')
+
+//VARIABLER FÖR FELMEDDELANDE
+const namnFel = document.getElementById('namnFel')
+const epostFel = document.getElementById('epostFel')
+const telFel = document.getElementById('telFel')
+const meddelandeFel = document.getElementById('meddelandeFel')
+//LYCKAD IFYLLNING SAMT SKICKAD
+const formSkickad = document.getElementById('formSkickad')
+
+//VALIDERINGSFUNKTIONER
+function valideraNamn(){
+    const namnInput = namnInput.value.trim()
+}
